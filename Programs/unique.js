8a6f228d467d45d2479b0a943a96a129fd66da50
Ankit kumar <ak8068716@gmail.com>
2023-01-03 20:45:44 +0530

    To find the unique number in an array

--- Programs/unique.js
+++ Programs/unique.js
@@ -1,3 +1,4 @@
+// here we are finding the unique number
 function unique(arr,n){
     let res = arr[0];
     for(let i = 1; i < n; i++){
@@ -6,8 +7,6 @@ function unique(arr,n){
     console.log(res);
 
 }
-
-
 
 let n =5
 let arr = [3,2,1,2,3,2]
